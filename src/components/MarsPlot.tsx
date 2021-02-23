1638d2a7f3e6acb87888ed3e7e7178ac64f8542c
Timothy Gramnaes <Tgramnaes@gmail.com>
2021-02-23 20:42:40 +0100

    Font Awesome working

--- src/components/MarsPlot.tsx
+++ src/components/MarsPlot.tsx
@@ -1,19 +1,46 @@
 
 import React, { CSSProperties } from 'react';
 
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
 function MarsPlot() {
+
     return(
-        <div style={mainContainer}>
+        <>
+        <div style={header}>
+        <FontAwesomeIcon icon={faBars} />
             <h2>Properties on Mars</h2>
+            <span></span>
 
         </div>
+        <div style={mainContainer}>
+
+        </div>
+        </>
 
     );
 
 }
 
-const mainContainer: CSSProperties = {
+const header: CSSProperties = {
     background: '#8A2424',
+    color: 'white',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    padding: '0 1rem'
+
+}
+
+
+const mainContainer: CSSProperties = {
+    background: '#E5E5E5',
+    display: 'flex',
+    marginTop: '4rem',
+    height: '100%',
+    width: 'auto'
+
 
 }
 
