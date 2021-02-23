a766eaa7e3fab51e461604541a70533281e238f3
Timothy Gramnaes <Tgramnaes@gmail.com>
2021-02-23 20:02:35 +0100

    first iterations

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Rover from "./components/Rover";
+import MarsPlot from "./components/MarsPlot"
 
 function App() {
   return (
     <div className="App">
       <Rover />
+      <MarsPlot />
     </div>
   );
 }
--- src/components/MarsPlot.tsx
+++ src/components/MarsPlot.tsx
@@ -1,2 +1,21 @@
 
-export {}
+import React, { CSSProperties } from 'react';
+
+function MarsPlot() {
+    return(
+        <div style={mainContainer}>
+            <h2>Properties on Mars</h2>
+
+        </div>
+
+    );
+
+}
+
+const mainContainer: CSSProperties = {
+    background: '#8A2424',
+
+}
+
+
+export default MarsPlot;
