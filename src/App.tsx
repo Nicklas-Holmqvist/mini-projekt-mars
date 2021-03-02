8e9029e4474aaf0e611b98dc4b6e6d246c04d861
Timothy Gramnaes <Tgramnaes@gmail.com>
2021-03-02 14:45:03 +0100

    minor fixings

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import React from "react";
-import Rover from "./components/Rover";
-import MarsPlot from "./components/MarsPlot";
-import LandingPage from "./components/LandingPage";
-import ErrorBoundary from "./components/ErrorBoundary";
+// import Rover from "./components/Rover";
+// import MarsPlot from "./components/MarsPlot";
+// import LandingPage from "./components/LandingPage";
+// import ErrorBoundary from "./components/ErrorBoundary";
 
 import { BrowserRouter } from "react-router-dom";
 // import Rover from "./components/Rover";
