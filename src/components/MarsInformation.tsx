d5534f95f78ccb7a856dd4428334332c7dfc79a0
Oliver Nygren <oliver.nygren@icloud.com>
2021-03-03 14:09:48 +0100

    working button

--- src/components/MarsInformation.tsx
+++ src/components/MarsInformation.tsx
@@ -1,33 +1,38 @@
 import React, { CSSProperties } from 'react';
+import { BrowserRouter, Link, Route } from 'react-router-dom';
+import Button from './Button';
+import ErrorBoundary from './ErrorBoundary';
+import MoreMarsInfo from './MoreMarsInfo';
 
 function MarsInformation() {
     return(
-        <>
-        <div style={marsContainer}>
+        <BrowserRouter>
+            <ErrorBoundary>
+                <div style={marsContainer}>
 
-        <h2>
-            Mars
-        </h2>
-        <span>
+                    <h2 style={h2Style}>Mars</h2>
 
-        </span>
+                    <span style={textStyle}>
+                        Mars is the fourth planet from the Sun and the second-smallest planet in the Solar System, being larger than only Mercury. In English, Mars carries the name of the Roman god of war and is often referred to as the "Red Planet".<br/><br/> The latter refers to the effect of the iron oxide prevalent on Mars's surface, which gives it a reddish appearance distinctive among the astronomical bodies visible to the naked eye.<br/><br/> Mars is a terrestrial planet with a thin atmosphere, with surface features reminiscent of the impact craters of the Moon and the valleys, deserts and polar ice caps of Earth.
 
-        <span>
-        Mars is the fourth planet from the Sun and the second-smallest planet in the Solar System, being larger than only Mercury. In English, Mars carries the name of the Roman god of war and is often referred to as the "Red Planet".<br/><br/> The latter refers to the effect of the iron oxide prevalent on Mars's surface, which gives it a reddish appearance distinctive among the astronomical bodies visible to the naked eye.<br/><br/> Mars is a terrestrial planet with a thin atmosphere, with surface features reminiscent of the impact craters of the Moon and the valleys, deserts and polar ice caps of Earth.
+                        The days and seasons are comparable to those of Earth, because the rotational period as well as the tilt of the rotational axis relative to the ecliptic plane are similar.<br/><br/> Mars is the site of Olympus Mons, the largest volcano and highest known mountain on any planet in the Solar System, and of Valles Marineris, one of the largest canyons in the Solar System. The smooth Borealis basin in the Northern Hemisphere covers 40% of the planet and may be a giant impact feature.<br/><br/> Mars has two moons, Phobos and Deimos, which are small and irregularly shaped. These may be captured asteroids, similar to 5261 Eureka, a Mars trojan.<br/>
 
-The days and seasons are comparable to those of Earth, because the rotational period as well as the tilt of the rotational axis relative to the ecliptic plane are similar.<br/><br/> Mars is the site of Olympus Mons, the largest volcano and highest known mountain on any planet in the Solar System, and of Valles Marineris, one of the largest canyons in the Solar System. The smooth Borealis basin in the Northern Hemisphere covers 40% of the planet and may be a giant impact feature.<br/><br/> Mars has two moons, Phobos and Deimos, which are small and irregularly shaped. These may be captured asteroids, similar to 5261 Eureka, a Mars trojan.<br/>
+                        Mars has been explored by several uncrewed spacecraft. Mariner 4 was the first spacecraft to visit Mars; launched by NASA on 28 November 1964, it made its closest approach to the planet on 15 July 1965. Mariner 4 detected the weak Martian radiation belt, measured at about 0.1% that of Earth, and captured the first images of another planet from deep space.<br/><br/> The Soviet Mars 3 mission included a lander, which achieved a soft landing in December 1971; however, contact was lost seconds after touchdown.<br/> On 20 July 1976, Viking 1 performed the first successful landing on the Martian surface.<br/> On 4 July 1997, the Mars Pathfinder spacecraft landed on Mars and on 5 July released its rover, Sojourner, the first robotic rover to operate on Mars.<br/> The Mars Express orbiter, the first European Space Agency (ESA) spacecraft to visit Mars, arrived in orbit on 25 December 2003.<br/><br/> In January 2004, NASA's Mars Exploration Rovers, named Spirit and Opportunity, both landed on Mars; Spirit operated until 22 March 2010 and Opportunity lasted until 10 June 2018.<br/><br/> NASA landed its Curiosity rover on August 6, 2012, as a part of its Mars Science Laboratory (MSL) mission to investigate Martian climate and geology.<br/><br/> On 24 September 2014, the Indian Space Research Organisation (ISRO) became the fourth space agency to visit Mars when its maiden interplanetary mission, the Mars Orbiter Mission spacecraft, arrived in orbit.<br/> The United Arab Emirates became the fifth to successfully undertake a mission to Mars, having inserted an orbiter in to the Martian atmosphere on 9 February 2021.<br/><br/> NASA's Perseverance rover successfully landed on Mars on 18 February 2021.
 
-Mars has been explored by several uncrewed spacecraft. Mariner 4 was the first spacecraft to visit Mars; launched by NASA on 28 November 1964, it made its closest approach to the planet on 15 July 1965. Mariner 4 detected the weak Martian radiation belt, measured at about 0.1% that of Earth, and captured the first images of another planet from deep space.<br/><br/> The Soviet Mars 3 mission included a lander, which achieved a soft landing in December 1971; however, contact was lost seconds after touchdown.<br/> On 20 July 1976, Viking 1 performed the first successful landing on the Martian surface.<br/> On 4 July 1997, the Mars Pathfinder spacecraft landed on Mars and on 5 July released its rover, Sojourner, the first robotic rover to operate on Mars.<br/> The Mars Express orbiter, the first European Space Agency (ESA) spacecraft to visit Mars, arrived in orbit on 25 December 2003.<br/><br/> In January 2004, NASA's Mars Exploration Rovers, named Spirit and Opportunity, both landed on Mars; Spirit operated until 22 March 2010 and Opportunity lasted until 10 June 2018.<br/><br/> NASA landed its Curiosity rover on August 6, 2012, as a part of its Mars Science Laboratory (MSL) mission to investigate Martian climate and geology.<br/><br/> On 24 September 2014, the Indian Space Research Organisation (ISRO) became the fourth space agency to visit Mars when its maiden interplanetary mission, the Mars Orbiter Mission spacecraft, arrived in orbit.<br/> The United Arab Emirates became the fifth to successfully undertake a mission to Mars, having inserted an orbiter in to the Martian atmosphere on 9 February 2021.<br/><br/> NASA's Perseverance rover successfully landed on Mars on 18 February 2021.
+                        There are investigations assessing the past habitability of Mars, as well as the possibility of extant life. Astrobiology missions are planned, such as the European Space Agency's Rosalind Franklin rover.<br/><br/> Liquid water on the surface of Mars cannot exist due to low atmospheric pressure, which is less than 1% of the atmospheric pressure on Earth, except at the lowest elevations for short periods.<br/><br/> The two polar ice caps appear to be made largely of water. The volume of water ice in the south polar ice cap, if melted, would be sufficient to cover the planetary surface to a depth of 11 metres (36 ft).<br/> In November 2016, NASA reported finding a large amount of underground ice in the Utopia Planitia region. The volume of water detected has been estimated to be equivalent to the volume of water in Lake Superior.<br/><br/>
 
-There are investigations assessing the past habitability of Mars, as well as the possibility of extant life. Astrobiology missions are planned, such as the European Space Agency's Rosalind Franklin rover.<br/><br/> Liquid water on the surface of Mars cannot exist due to low atmospheric pressure, which is less than 1% of the atmospheric pressure on Earth, except at the lowest elevations for short periods.<br/><br/> The two polar ice caps appear to be made largely of water. The volume of water ice in the south polar ice cap, if melted, would be sufficient to cover the planetary surface to a depth of 11 metres (36 ft).<br/> In November 2016, NASA reported finding a large amount of underground ice in the Utopia Planitia region. The volume of water detected has been estimated to be equivalent to the volume of water in Lake Superior.<br/><br/>
-
-Mars can easily be seen from Earth with the naked eye, as can its reddish coloring. Its apparent magnitude reaches −2.94, which is surpassed only by Venus, the Moon and the Sun.<br/> Optical ground-based telescopes are typically limited to resolving features about 300 kilometres (190 mi) across when Earth and Mars are closest because of Earth's atmosphere.<br/>
-<br/>
-<br/>
-Collected from https://en.wikipedia.org/wiki/Mars
-        </span>
-        </div>
-        </>
+                        Mars can easily be seen from Earth with the naked eye, as can its reddish coloring. Its apparent magnitude reaches −2.94, which is surpassed only by Venus, the Moon and the Sun.<br/> Optical ground-based telescopes are typically limited to resolving features about 300 kilometres (190 mi) across when Earth and Mars are closest because of Earth's atmosphere.<br/>
+                        <br/>
+                        <br/>
+                        Collected from https://en.wikipedia.org/wiki/Mars
+                    </span>
+                    <Link to="/MoreMarsInfo">
+                        <Button btnText="Läs Mer" />
+                    </Link>
+                    <Route path="/MoreMarsInfo" component={MoreMarsInfo} />
+                </div>
+            </ErrorBoundary>
+        </BrowserRouter>
     )
 
 }
@@ -36,9 +41,19 @@ const marsContainer: CSSProperties = {
     marginTop: '2rem',
     padding: '3rem',
     height: '100%',
-    width: 'auto'
+    width: 'auto',
+    display: 'flex',
+    flexDirection: 'column'
+}
 
+const h2Style: CSSProperties = {
+    color: 'white',
+    fontSize: '2.5rem'
+}
 
+const textStyle: CSSProperties = {
+    color: 'white',
+    marginBottom: '2rem'
 }
 
 export default MarsInformation;
